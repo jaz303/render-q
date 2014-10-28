68820c581b5231c0722a56b0d434cd8859b245d1
Jason Frame <jason@onehackoranother.com>
2014-10-28 10:25:15 +0000

    tests, docs

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 # render-q
 
+Schedule rendering operations for future batch application with `requestAnimationFrame()`.
+
 ## API
 
+#### `q = renderq(window)`
+
+Create a new queue bound to `window`.
+
+#### `q(fn)`
+
+Enqueue render function `fn` to be called inside the next `requestAnimationFrame()` callback. If a rendering batch is currently in process the function will be invoked immediately.
+
+#### `q.after(fn)`
+
+Enqueue `fn` to be called after the current batch of render callbacks (i.e. those enqueued via `q()`) have been completed.
+
+#### `q.later(fn)`
+
+Enqueue render function `fn` to be called later inside a `requestAnimationFrame()` callback. `q.later()` differs from `q()` in that it will never invoke `fn` immediately, and always schedule it for the next batch - even if a rendering batch is currently active. Useful for scheduling animations.
+
+#### `q.isRendering()`
+
+Returns `true` if this queue is currently processing a batch of render callbacks.
--- test/main.js
+++ test/main.js
@@ -11,18 +11,49 @@ window.init = function() {
 		});
 	}
 
-	t('operation ordering', function(assert, q) {
+	t('enqueued function is called asynchronously', function(assert, q) {
 		
 		var out = [];
+		
+		q(function() { out.push(1); });
+		assert.deepEquals(out, []);
 
-		q.after(function() { out.push(3); });
+		setTimeout(function() {
+			assert.deepEquals(out, [1]);
+			assert.end();
+		}, 200);
+
+	});
+
+	t('after() function is called after rendering', function(assert, q) {
+
+		var out = [];
+
+		q.after(function() {
+			assert.deepEquals(out, [1]);
+			out.push(2);
+		});
+		
+		q(function() { out.push(1); });
+
+		setTimeout(function() {
+			assert.deepEquals(out, [1,2]);
+			assert.end();	
+		}, 200);
+
+	});
+
+	t('enqueued functions are called in order', function(assert, q) {
+
+		var out = [];
+		
 		q(function() { out.push(1); });
 		q(function() { out.push(2); });
 
-		setTimeout(function() {
-			assert.deepEquals(out, [1,2,3]);
-			assert.end();	
-		}, 200);
+		q.after(function() {
+			assert.deepEquals(out, [1,2]);
+			assert.end();
+		});
 
 	});
 
@@ -33,6 +64,34 @@ window.init = function() {
 			assert.deepEquals(out, [1]);
 			assert.end();
 		});
+	});
+
+	t('q.later() outwith rendering will be applied by first render loop', function(assert, q) {
+
+		var out = [];
+		
+		q.later(function() { out.push(1); });
+
+		q.after(function() {
+			assert.deepEquals(out, [1]);
+			assert.end();
+		});
+
+	});
+
+	t('q.later() during rendering is deferred until next render loop', function(assert, q) {
+		
+		var out = [];
+		q(function() {
+			out.push(1);
+			q.later(function() { out.push(3); });
+			q.after(function() { out.push(2); });
+		});
+
+		setTimeout(function() {
+			assert.deepEquals(out, [1,2,3]);
+			assert.end();
+		}, 200);
 	});
 
 	t('isRendering() is false when not rendering', function(assert, q) {
